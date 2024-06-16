20cd4deba3a1ffa55ef40e1afd770d96d08d42ee
wtsytcx <tea32568@gmail.com>
2024-06-16 19:57:36 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2435,10 +2435,10 @@ worldmap[298] = {group: 298, dif: 1, name: '298', map: '4252', route: [252, 239]
 worldmap[299] = {group: 299, dif: 1, name: '299', map: '5171234354567686', route: [279, 218, 225, 218, 247, 236, 237, 207]};
 worldmap[300] = {group: 300, dif: 1, name: '300', map: '516264552696', route: [248, 228, 217, 249, 269, 244]};
 //ТЕСТ
-worldmap[104699] = {group: 104698, dif: 1, name: '', parent: 80, route: []};
-worldmap[104710] = {group: 104710, dif: 1, name: 'Лужайка душистых цветов', map: 'Лужайка душистых цветовКП', route: [132056, 100415]};
-worldmap[132056] = {group: 132056, dif: 1, name: 'Потаённый туннель', map: 'Потаённый туннельКП', route: [132056]};
-worldmap[100415] = {group: 100415, dif: 1, name: 'Вересковая пустошь', map: 'Вересковая пустошьКП', route: [10708, 104709, 104710]};
+worldmap[104698] = {group: 104698, dif: 1, name: '', parent: 80, route: []};
+worldmap[104699] = {group: 104710, dif: 1, name: 'Лужайка душистых цветов', map: 'Лужайка душистых цветовКП', route: [132056, 100415]};
+worldmap[104700] = {group: 132056, dif: 1, name: 'Потаённый туннель', map: 'Потаённый туннельКП', route: [132056]};
+worldmap[104701] = {group: 100415, dif: 1, name: 'Вересковая пустошь', map: 'Вересковая пустошьКП', route: [10708, 104709, 104710]};
 
 makeHiddenListIDbyMap(200, 200, 300, true);
 makeHiddenListIDbyMap(73000, 7000, 7999, true, 23507, true);
